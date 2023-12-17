74705a8f96d57a4eeee70dac69ccc278beb19ae8
Devrath <devrath.dev595@gmail.com>
2023-12-17 19:19:15 +0530

    added more code

--- Code/lib-utils/build.gradle.kts
+++ Code/lib-utils/build.gradle.kts
@@ -1,20 +1,18 @@
 plugins {
-    id("com.android.application")
+    id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
 }
 
 android {
-    namespace = "com.istudio.utils"
-    compileSdk = 34
+    namespace = "com.istudio.lib_utils"
+    compileSdk = 33
 
     defaultConfig {
-        applicationId = "com.istudio.utils"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
     }
 
     buildTypes {
@@ -40,11 +38,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-
-    implementation(project(":lib-utils"))
 }
--- Code/settings.gradle.kts
+++ Code/settings.gradle.kts
@@ -15,3 +15,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "code"
 include(":app")
+include(":lib-utils")
