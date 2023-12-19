cbae02c6cf095ad7aa7cb155801c326cb76b1d5b
Devrath <devrath.dev595@gmail.com>
2023-12-19 08:54:05 +0530

    added

--- Code/app/src/main/java/com/istudio/utils/MyApplication.kt
+++ Code/app/src/main/java/com/istudio/utils/MyApplication.kt
@@ -1,14 +1,16 @@
 package com.istudio.utils
 
 import android.app.Application
-import androidx.startup.AppInitializer
-import com.istudio.lib_utils.sdkConfig.SDKInitializer
+//import androidx.startup.AppInitializer
+import com.istudio.lib_utils.sdkConfig.SDKConfig
+//import com.istudio.lib_utils.sdkConfig.SDKInitializer
 
 class MyApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        AppInitializer.getInstance(this).initializeComponent(SDKInitializer::class.java)
+        SDKConfig.initContext(this)
+        //AppInitializer.getInstance(this).initializeComponent(SDKInitializer::class.java)
     }
 
 }
